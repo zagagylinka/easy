c6c0d767f644abf92efff67f28235337a6c79df2
zagagylinka <159018289+zagagylinka@users.noreply.github.com>
2024-02-05 14:58:55 +0300

    Add files via upload

---
+++ .dota2.h
@@ -0,0 +1,6 @@
+#ifndef _DOTA2_H_INCLUDED
+#define _DOTA2_H_INCLUDED
+
+int summ(int a, int b);
+
+#endif // _DOTA2_H_INCLUDED
---
+++ _dota2_.cpp
@@ -0,0 +1,5 @@
+int summ(int a, int b){
+    int c;
+    c = a + b;
+    return c;
+}
